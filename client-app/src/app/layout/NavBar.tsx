24576e15c95193fb812086dc45e4df671750dd28
Łukasz Tyczka <lukasz@tyczka.net>
2021-07-11 19:20:12 +0200

    Client crud operation

---
+++ client-app/src/app/layout/NavBar.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Menu, Button } from 'semantic-ui-react';
+
+interface Props {
+    openForm: () => void;
+}
+
+export default function NavBar({ openForm }: Props) {
+    return (
+        <Menu inverted fixed="top">
+            <Container>
+                <Menu.Item header>
+                    <img
+                        src="./assets/logo.png"
+                        alt="Logo"
+                        style={{ marginRight: 10 }}
+                    />
+                    Reactivities
+                </Menu.Item>
+                <Menu.Item name="Activities" />
+                <Menu.Item>
+                    <Button
+                        onClick={openForm}
+                        positive
+                        content="Create Activity"></Button>
+                </Menu.Item>
+            </Container>
+        </Menu>
+    );
+}
